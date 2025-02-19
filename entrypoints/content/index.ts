5e3f40f64ab0de4eb9365f770a98d418b8a8c737
Adolein <ado.it.mu@gmail.com>
2025-02-19 11:18:30 +0100

    Read From Dom

--- entrypoints/content/index.ts
+++ entrypoints/content/index.ts
@@ -1,15 +1,36 @@
+import { ContentScriptContext } from "wxt/client";
+import App from "./App.vue";
+import { createApp } from "vue";
+import "./reset.css";
+
 export default defineContentScript({
+
     //matches: ['*://*.google.com/*'],
     matches: ['*://*.amazon.de/*'],
     //https://www.amazon.de/dp/B01JLGWGEQ
+    cssInjectionMode: "ui",
+
     async main(ctx) {
+        const ui = await defineOverlay(ctx);
+
+        // Mount initially
+        ui.mount();
+
+        // Re-mount when page changes
+        ctx.addEventListener(window, "wxt:locationchange", (event) => {
+            ui.mount();
+        });
+
+
+        ////////////////
+        // Vanilla JS Method
+        ////
         console.log('Hello content.');
         console.log("Injecting script...");
         await injectScript("/injected.js", {
             keepInDom: true,
         });
         console.log("Done!");
-        // Fügt ein div zur Seite hinzu
         const newDiv = document.createElement("div");
         newDiv.id = "my-wxt-div";
         newDiv.style.position = "fixed";
@@ -19,7 +40,36 @@ export default defineContentScript({
         newDiv.style.color = "white";
         newDiv.style.padding = "10px";
         newDiv.style.borderRadius = "5px";
-        newDiv.innerText = "Mein Dev";
+        newDiv.innerText = "Vanilla JS implementiert";
         document.body.appendChild(newDiv);
+
+        // DOM analysieren
+        const titleElement = document.querySelector("#productTitle");
+        const title = titleElement?.textContent?.trim() || "Kein Titel gefunden";
+        console.log("Produkt-Titel:", title);
+
+        const priceElement = document.querySelector(".a-price .a-offscreen");
+        const price = priceElement?.textContent?.trim() || "Kein Preis gefunden";
+        console.log("Preis:", price);
+
+        console.log(document)
+
     },
 });
+
+function defineOverlay(ctx: ContentScriptContext) {
+    return createShadowRootUi(ctx, {
+        name: "vue-overlay",
+        position: "modal",
+        zIndex: 99999,
+        onMount(container, _shadow, shadowHost) {
+            const app = createApp(App);
+            app.mount(container);
+            shadowHost.style.pointerEvents = "none";
+            return app;
+        },
+        onRemove(app) {
+            app?.unmount();
+        },
+    });
+}
