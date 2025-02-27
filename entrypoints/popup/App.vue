3fed4350a4c6e43399121b7d3b3f6ea6e1428611
Adolein <ado.it.mu@gmail.com>
2025-02-27 12:06:12 +0100

    design updated

--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -4,7 +4,7 @@ import Tabs from '@/components/Tabs/Tabs.vue';
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center bg-gray-100">
+  <div class="flex flex-col items-center justify-center bg-gray-100 min-w-[350px]">
     <Tabs />
   </div>
 
